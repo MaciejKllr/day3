79d1d043f4ace68c4acad322c00c8aef4b01c394
Macie Keller <maciej.keller66@gmail.com>
2024-12-04 15:18:22 +0000

    final version

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,40 @@
+use std::cell::RefCell;
+
+use crate::blog::Blog;
+
+mod blog;
+
+thread_local! {
+    static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
+}
+// komentarze ?
+
+#[ic_cdk::update]
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
+    if title.len() > 250 {
+        return Err("Title is too long!".to_string())
+    }
+    if content.len() > 2000 {
+      return Err("Too much content!".to_string())
+    }
+    if tags.len() > 3 {
+      return Err("Too many tags!".to_string())
+    }
+    let blog = Blog::new(title, content, tags);
+    BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
+    let last_blog = BLOGS.with(|blogs| 
+        blogs
+        .borrow()
+        .last()
+        .expect("Vec should not be empty").clone());
+    Ok(last_blog)
+}
+
+#[ic_cdk::query]
+fn get_blogs() -> Vec<Blog> {
+    BLOGS.with(|blogs| blogs.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
